7f6b9a1718547e3bffad8bb307f5f169e610574a
zhanpei <zhangzhanpei@gmail.com>
2017-06-27 20:48:46 +0800

    分页组件

---
+++ components/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <posts></posts> 
+  </div>
+</template>
+
+<script>
+const posts = require("./views/posts.vue");
+export default {
+  name: 'app',
+  components: {
+    posts
+  }
+}
+</script>
---
+++ components/src/views/posts.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <paginate :page.sync="page" :totalPages="totalPages"></paginate>
+    </div>
+</template>
+<script>
+const paginate = require("../components/paginate.vue");
+export default {
+  data () {
+    return {
+      page: 1,
+      totalPages: 20
+    }
+  },
+  components: {
+    paginate
+  }
+}
+</script>
