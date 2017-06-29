ef7b8f4e863de3d3a7a5adcfe6650aa04a665d7b
zhanpei <zhangzhanpei@gmail.com>
2017-06-29 21:43:17 +0800

    获取当前url的path

--- components/src/App.vue
+++ components/src/App.vue
@@ -2,9 +2,9 @@
   <div id="app" class="col-md-8 col-md-offset-2">
     <nav>
       <ul class="nav nav-pills">
-        <li role="presentation"><router-link to="/">首页</router-link></li>
-        <li role="presentation"><router-link to="/posts">文章</router-link></li>
-        <li role="presentation"><router-link to="/transition">过渡</router-link></li>
+        <li role="presentation" :class="{active: $route.path === '/'}"><router-link to="/">首页</router-link></li>
+        <li role="presentation" :class="{active: $route.path === '/posts'}"><router-link to="/posts">文章</router-link></li>
+        <li role="presentation" :class="{active: $route.path === '/transition'}"><router-link to="/transition">过渡</router-link></li>
       </ul>
     </nav>
     <div>
